06828a167584eefd7dcba6ece0e104c3d7c17c10
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-03-11 23:25:32 +0600

    Add files via upload

---
+++ variable_swapping.py
@@ -0,0 +1,13 @@
+from time import sleep
+a =( input("Enter the value of first number a = "))
+b =(input ("Enter the value of seconf number b = "))
+#Swapping without a temporary variable
+a,b = b,a
+print ("After Swapping : ")
+print ("W8 a momment \t \t")
+sleep (1)
+print ("Lemme guess uhnn :) ")
+sleep (2)
+
+print ("a =",a)
+print ("b =",b)
