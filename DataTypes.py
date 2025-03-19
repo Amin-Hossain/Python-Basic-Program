efb8dd872bfb4f6258b3402baad6a0dafb881f26
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-03-19 14:48:46 +0600

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,22 @@
+#int data type
+age = 22
+print (type(age)) #this is defined which data types it is
+print (age)
+
+#str data type
+name = "Amin"
+print (type (name))
+print (name)
+
+#float data type
+float = 40.2
+print (type (float))
+print (float)
+
+#complex data type
+"""In every complex data type.. 'j' is must be the end """
+nmbr = 01955924272j
+print (type (nmbr))
+print (nmbr)
+
+#without 'j' code is error .
