bb1fa54f674b2d4680ef94d45bcd4b5f120c71f9
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-03-19 14:42:35 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,9 @@
+'''A variable is a placeholder for information you want Python to recall later in the coding
+process when you need to complete an action'''
+
+name = "Amin" #In Define Characters.We use " " ..This is only for characters
+age = 22 #In Define Integer.We do not use " ".
+print (name,age)
+print ("I am a Egnineering Student") #In  this line.The output show is AMIN because we use quatation in parentheses.That is why python understand that is a output
+
+#sorry for my english .. i have some grammatical problem :)
